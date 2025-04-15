e3a77850c93bdef9ae635e3f09f821ba68299e10
unknown <saivishwaram.ramkumar@gmail.com>
2025-04-15 23:48:37 +0530

    reset password is set

--- Backend/Auth/forgotPassword.js
+++ Backend/Auth/forgotPassword.js
@@ -1,5 +1,13 @@
 async function forgotPassword(req,res) {
-    
+    try{
+        
+    }
+    catch(err){
+        console.log(err)
+        res.status(200).json({
+            err:"internal error..."
+        })
+    }
 }
 
 module.exports = {
