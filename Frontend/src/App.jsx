5f6a3c98682b761c9b48bccd4c3316ed76608606
santhoshkumar011 <220701253@rajalakshmi.edu.in>
2025-04-16 09:15:22 +0530

    Updated Login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LoginPage from './pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-4xl bg-red-500 text-white'>
-      Hlo
-    </div>
+    <LoginPage/>
+    // <div className='text-4xl bg-red-500 text-white'>
+    //   Hlo
+    // </div>
   )
 }
 
